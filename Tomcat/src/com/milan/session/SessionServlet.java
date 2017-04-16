6e49ecf0b7b9a3031eeebbb68ccc93489a54048d
ClarkKentYang <lykaka22@qq.com>
2017-04-16 21:58:33 +0800

    add session

---
+++ Tomcat/src/com/milan/session/SessionServlet.java
@@ -0,0 +1,43 @@
+package com.milan.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 HttpSession session = request.getSession();//session创建
+		 System.out.println("JsessionId"+session.getId());
+		 //session销毁：默认30分钟自动销毁，服务器非正常关闭，session.invalidate();
+		 session.invalidate();//销毁session
+		 System.out.println("JsessionId"+session.getId());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
