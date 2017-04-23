d4c4fc1a86de0ea58f2f86ef9fe29ac6d38c5c18
ClarkKentYang <lykaka22@qq.com>
2017-04-23 23:01:20 +0800

    update login

---
+++ Tomcat/src/com/milan/servlet/LoginServlet.java
@@ -0,0 +1,48 @@
+package com.milan.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<String> userList= new ArrayList<>();//模拟数据库
+		userList.add("kaka");
+		userList.add("shewa");
+		userList.add("Inzaghi");
+		String username = request.getParameter("username");
+		boolean exist = userList.contains(username);
+		response.getWriter().write("{\"exist\":"+ exist +"}");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
